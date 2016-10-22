4337de771e697d1f8389cf941523c3fe3d07de54
Lab103 <Lab103@LAB101-01>
2016-10-22 11:26:33 -0200

    Aula 22/10

--- sceweb/src/br/sceweb/controle/ServletControle.java
+++ sceweb/src/br/sceweb/controle/ServletControle.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 
+import br.sceweb.modelo.Convenio;
+import br.sceweb.modelo.ConvenioDAO;
 import br.sceweb.modelo.Empresa;
 import br.sceweb.modelo.EmpresaDAO;
 
@@ -79,14 +81,14 @@ public class ServletControle extends HttpServlet {
 			try {
 				if (!cnpj.isEmpty()) {
 					empresa = consulta(cnpj);
-					if (empresa != null){
+					if (empresa != null) {
 						logger.info("consulta empresa nome da empresa  = " + empresa.getNomeDaEmpresa());
 						request.setAttribute("nomeDaEmpresa", empresa.getNomeDaEmpresa());
-					    request.setAttribute("cnpj", empresa.getCnpj());
-					    request.setAttribute("nomeFantasia", empresa.getNomeFantasia());
-					    request.setAttribute("endereco", empresa.getEndereco());
-					    request.setAttribute("telefone", empresa.getTelefone());
-					    request.setAttribute("msg", "");
+						request.setAttribute("cnpj", empresa.getCnpj());
+						request.setAttribute("nomeFantasia", empresa.getNomeFantasia());
+						request.setAttribute("endereco", empresa.getEndereco());
+						request.setAttribute("telefone", empresa.getTelefone());
+						request.setAttribute("msg", "");
 						url = "/visao/FormEmpresaResultadoDaConsulta.jsp";
 					} else {
 						request.setAttribute("msg", "cnpj invalido");
@@ -98,13 +100,42 @@ public class ServletControle extends HttpServlet {
 				logger.info(e.getMessage() + e.getCause());
 			}
 			request.getRequestDispatcher(url).forward(request, response);
-			
+
 		}
 		if (parametro.equals("ExcluirEmpresa")) {
 			url = "/visao/FormEmpresa.jsp";
 			try {
 				resultado = excluirEmpresa(request.getParameter("txtCNPJ"));
 				logger.info("resultado do cadastra = " + resultado);
+				request.setAttribute("msg", resultado);
+				request.getRequestDispatcher(url).forward(request, response);
+			} catch (Exception e) {
+				request.setAttribute("msg", resultado);
+				request.getRequestDispatcher(url).forward(request, response);
+				logger.info("erro  = " + e.getMessage());
+
+			}
+		}
+		if (parametro.equals("IncluirConvenio")) {
+			url = "/visao/FormConvenio.jsp";
+			try {
+				resultado = cadastrarConvenio(request.getParameter("txtCNPJ"), request.getParameter("txtDtInicio"),
+						request.getParameter("txtDtTermino"));
+				logger.info("resultado do cadastra convenio= " + resultado);
+				request.setAttribute("msg", resultado);
+				request.getRequestDispatcher(url).forward(request, response);
+			} catch (Exception e) {
+				request.setAttribute("msg", resultado);
+				request.getRequestDispatcher(url).forward(request, response);
+				logger.info("erro  = " + e.getMessage());
+
+			}
+		}
+		if (parametro.equals("ExcluirConvenio")) {
+			url = "/visao/FormConvenio.jsp";
+			try {
+				resultado = excluirConvenio(request.getParameter("txtCNPJ"));
+				logger.info("resultado da exclusao = " + resultado);
 				request.setAttribute("msg", resultado);
 				request.getRequestDispatcher(url).forward(request, response);
 			} catch (Exception e) {
@@ -154,6 +185,42 @@ public class ServletControle extends HttpServlet {
 			msg = e.getMessage();
 		}
 
+		return msg;
+	}
+
+	public String excluirConvenio(String cnpj) {
+		String msg = "";
+		int rc = 0;
+		ConvenioDAO convenioDAO = new ConvenioDAO();
+		try {
+			rc = convenioDAO.exclui(cnpj);
+			if (rc == 1)
+				msg = "excluido com sucesso";
+			else
+				msg = "erro na exclusão";
+		} catch (Exception e) {
+			msg = e.getMessage();
+		}
+
+		return msg;
+	}
+
+	public String cadastrarConvenio(String cnpj, String dataInicio, String dataTermino) {
+		String msg = "";
+		int codigoRetorno = 0;
+		ConvenioDAO convenioDAO = new ConvenioDAO();
+		logger.info("cadastrar convenio = " + cnpj);
+		try {
+			Convenio convenio = new Convenio(cnpj, dataInicio, dataTermino);
+			codigoRetorno = convenioDAO.adiciona(convenio);
+			if (codigoRetorno == 0) {
+				msg = "erro - cadastro nao realizado ";
+			} else {
+				msg = "cadastro realizado com sucesso";
+			}
+		} catch (Exception e) {
+			msg = e.getMessage();
+		}
 		return msg;
 	}
 }
